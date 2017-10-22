814d7294682e3bf9fe533381268c802e069c4134
mammarelm7 <mammarelm7@students.rowan.edu>
2017-10-22 18:58:02 -0400

    added

--- Sensors and Signal Conditioning/MSP430G2553/blink.c
+++ Sensors and Signal Conditioning/MSP430G2553/blink.c
@@ -11,67 +11,86 @@ ECE 09342-2
 
 unsigned int ADC_value=0;
 
-void ConfigureAdc(void);
+void initAdc(void);
 
 void main(void)
 {
     {
-        WDTCTL = WDTPW + WDTHOLD;       // Stop WDT
+        // Stop WDT
+        WDTCTL = WDTPW + WDTHOLD;
 
-        if (CALBC1_1MHZ==0xFF)                    // If calibration constant erased
-               {
-                 while(1);                               // do not load, trap CPU!!
-               }
-               DCOCTL = 0;                               // Select lowest DCOx and MODx settings
-               BCSCTL1 = CALBC1_1MHZ;          // Set range   DCOCTL = CALDCO_1MHZ;
-               DCOCTL = CALDCO_1MHZ;
-               P1SEL = BIT1 + BIT2 ;                     // P1.1 = RXD, P1.2=TXD
-               P1SEL2 = BIT1 + BIT2 ;                    // P1.1 = RXD, P1.2=TXD
-               UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-               UCA0BR0 = 104;                            // 1MHz 9600
-               UCA0BR1 = 0;                              // 1MHz 9600
-               UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
-               UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-               IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+        // If calibration constant erased
+        if (CALBC1_1MHZ==0xFF)
+        {
+            // do not load
+                 while(1);
+         }
+        // Select lowest DCOx and MODx settings
+        DCOCTL = 0;
+        // Set range   DCOCTL = CALDCO_1MHZ;
+        BCSCTL1 = CALBC1_1MHZ;
+        DCOCTL = CALDCO_1MHZ;
+        // P1.1 = RXD, P1.2=TXD
+        P1SEL = BIT1 + BIT2 ;
+        // P1.1 = RXD, P1.2=TXD
+        P1SEL2 = BIT1 + BIT2 ;
+        // SMCLK
+        UCA0CTL1 |= UCSSEL_2;
+        // 1MHz 9600
+        UCA0BR0 = 104;
+        // 1MHz 9600
+        UCA0BR1 = 0;
+        // Modulation UCBRSx = 1
+        UCA0MCTL = UCBRS0;
+        // **Initialize USCI state machine**
+        UCA0CTL1 &= ~UCSWRST;
+        // Enable USCI_A0 RX interrupt
+        IE2 |= UCA0RXIE;
 
 
-
-        BCSCTL2 &= ~(DIVS_3);           // SMCLK = DCO = 1MHz
-        P1SEL |= BIT3;                  // ADC input pin P1.3
-        ConfigureAdc();                 // ADC set-up function call
-        __enable_interrupt();           // Enable interrupts.
-
-
-
-
-
+        // SMCLK = DCO = 1MHz
+        BCSCTL2 &= ~(DIVS_3);
+        // ADC input pin P1.3
+        P1SEL |= BIT3;
+        // ADC set-up function call
+        initAdc();
+        // Enable interrupts.
+        __enable_interrupt();
 
         while(1)
         {
-            __delay_cycles(1000);               // Wait for ADC Ref to settle
-            ADC10CTL0 |= ENC + ADC10SC;         // Sampling and conversion start
-            __bis_SR_register(CPUOFF + GIE);    // Low Power Mode 0 with interrupts enabled
-            ADC_value = ADC10MEM;               // Assigns the value held in ADC10MEM to the integer called ADC_value
+            // Wait for ADC Ref to settle
+            __delay_cycles(1000);
+            // Sampling and conversion start
+            ADC10CTL0 |= ENC + ADC10SC;
+            // Low Power Mode 0 with interrupts enabled
+            __bis_SR_register(CPUOFF + GIE);
+            //set adc value
+            ADC_value = ADC10MEM;
 
         }
 
     }
 }
 
-// ADC10 interrupt service routine
+//ADC10 interrupt
 #pragma vector=ADC10_VECTOR
 __interrupt void ADC10_ISR (void)
 {
-    __bic_SR_register_on_exit(CPUOFF);        // Return to active mode }
+    // Return to active mode
+    __bic_SR_register_on_exit(CPUOFF);
 }
 
-// Function containing ADC set-up
-void ConfigureAdc(void)
+//ADC set-up
+void initAdc(void)
 {
 
-    ADC10CTL1 = INCH_3 + ADC10DIV_3 ;         // Channel 3, ADC10CLK/3
-    ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON + ADC10IE;  // Vcc & Vss as reference, Sample and hold for 64 Clock cycles, ADC on, ADC interrupt enable
-    ADC10AE0 |= BIT3;                         // ADC input enable P1.3
+    // Channel 3, ADC10CLK/3
+    ADC10CTL1 = INCH_3 + ADC10DIV_3 ;
+    //Vcc and Vss reference, Sample and hold for 64 Clock cycles, ADC on, ADC interrupt enable
+    ADC10CTL0 = SREF_0 + ADC10SHT_3 + ADC10ON + ADC10IE;
+    // ADC input enable P1.3
+    ADC10AE0 |= BIT3;
 }
 
 
@@ -86,11 +105,12 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
 #error Compiler not supported!
 #endif
 {
-  while (!(IFG2&UCA0TXIFG));                // USCI_A0 TX buffer ready?
+  // USCI_A0 TX buffer ready?
+  while (!(IFG2&UCA0TXIFG));
 
   //if input is A print out the ADC
   if(UCA0RXBUF == 65){
-      UCA0TXBUF = ADC10MEM;                    // TX -> RXed character
+      UCA0TXBUF = ADC10MEM; //skips a clockcycle of setting adc_value
   }
   //else print out 66 
   else{
