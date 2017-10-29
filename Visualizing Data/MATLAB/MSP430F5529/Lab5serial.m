45337067512905b3fe349eb5bcbe0012b1f18995
mammarelm7 <mammarelm7@students.rowan.edu>
2017-10-29 16:44:14 -0400

    added

---
+++ Visualizing Data/MATLAB/MSP430F5529/Lab5serial.m
@@ -0,0 +1,56 @@
+%Receive Serial Sensor MSP430F5529
+%Gets 3 sensor reads over 3 seconds
+%increased max y limit to 260
+%Matt Mammarelli
+%9/27/17
+
+
+delete(instrfindall); %prevents cant use com port error
+
+clear all;
+clf
+Ts = 1;   % Sampling time
+t = 1:Ts:6; % support of signal
+
+p=serial('COM8','BaudRate',9600,'DataBits',8); %have to change com to current one
+set(p,'Parity','none');
+set(p,'Terminator','LF');
+fopen(p); %opens com port
+fprintf(p,'A') %sends character A to UART to receive sensor info
+
+
+pause(1); %delay one second
+fprintf(p,'A')
+
+
+pause(1); %delay one second
+fprintf(p,'A')
+
+
+veri=fscanf(p,'%u'); %gets UART sensor information
+
+figure %creates figure
+xlabel('Count')
+ylabel('Sense Change')
+title('Photoresistor Count vs. Sense')
+grid on;
+hold on;
+
+
+for t=1:Ts:6
+    if (mod(t,2)==1) %only use odd matrix indices, evens are return characters
+    plot(t,uint8(veri(t)),'o'); %plots circles 
+    end
+    xlim ([0 6]) %x limit
+    ylim ([0 260]) %y limit
+    drawnow    
+end
+
+    
+
+fclose(p); %closes serial connection
+delete(p); %deletes serial variable
+
+
+
+
---
+++ Visualizing Data/MATLAB/MSP430F5529/blink.c
@@ -0,0 +1,129 @@
+/*
+Matt Mammarelli
+9/18/17
+ECE 09342-2
+*/
+
+//MSP430F5529 Visualizing Data Matlab
+//Will transmit the current sensor value when character A is sent to processor over UART
+
+#include <msp430.h>
+
+unsigned int ADC_value=0;
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  ADC12CTL0 = ADC12SHT02 + ADC12ON;         // Sampling time, ADC12 on
+  ADC12CTL1 = ADC12SHP;                     // Use sampling timer
+  ADC12IE = 0x01;                           // Enable interrupt
+  ADC12CTL0 |= ADC12ENC;
+  P6SEL |= 0x01;                            // P6.0 ADC option select
+  P1DIR |= 0x01;                            // P1.0 output
+
+
+  //uart **************************************************************************************************
+  // P3.3, P3.4 transmit/receive
+  P3SEL = BIT3+BIT4;
+  // Put state machine in reset
+  UCA0CTL1 |= UCSWRST;
+  // SMCLK
+  UCA0CTL1 |= UCSSEL_2;
+  // 1MHz 9600 baud
+  UCA0BR0 = 6;
+  // 1MHz 9600
+  UCA0BR1 = 0;
+  //sets m control register
+  UCA0MCTL = UCBRS_0 + UCBRF_13 + UCOS16;
+  //sets control register
+  UCA0CTL1 &= ~UCSWRST;
+  //enable interrupt
+  UCA0IE |= UCRXIE;
+  //*******************************************************************************************************
+
+
+
+
+  while (1)
+  {
+    ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
+    __bis_SR_register(LPM0_bits + GIE);     // LPM0, ADC12_ISR will force exit
+    __no_operation();                       // For debugger
+  }
+}
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch(__even_in_range(ADC12IV,34))
+  {
+  case  0: break;                           // Vector  0:  No interrupt
+  case  2: break;                           // Vector  2:  ADC overflow
+  case  4: break;                           // Vector  4:  ADC timing overflow
+  case  6:                                  // Vector  6:  ADC12IFG0
+
+        ADC_value = ADC12MEM0;
+
+    __bic_SR_register_on_exit(LPM0_bits);   // Exit active CPU
+  case  8: break;                           // Vector  8:  ADC12IFG1
+  case 10: break;                           // Vector 10:  ADC12IFG2
+  case 12: break;                           // Vector 12:  ADC12IFG3
+  case 14: break;                           // Vector 14:  ADC12IFG4
+  case 16: break;                           // Vector 16:  ADC12IFG5
+  case 18: break;                           // Vector 18:  ADC12IFG6
+  case 20: break;                           // Vector 20:  ADC12IFG7
+  case 22: break;                           // Vector 22:  ADC12IFG8
+  case 24: break;                           // Vector 24:  ADC12IFG9
+  case 26: break;                           // Vector 26:  ADC12IFG10
+  case 28: break;                           // Vector 28:  ADC12IFG11
+  case 30: break;                           // Vector 30:  ADC12IFG12
+  case 32: break;                           // Vector 32:  ADC12IFG13
+  case 34: break;                           // Vector 34:  ADC12IFG14
+  default: break;
+  }
+}
+
+
+
+
+//uart interrupt vector
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+  case 0:break;   // Vector 0 - no interrupt
+  case 2:{
+      while (!(UCA0IFG&UCTXIFG));  // USCI_A0 TX buffer check
+
+      //if input is A print out the ADC
+        if(UCA0RXBUF == 65){
+            UCA0TXBUF = ADC_value; //skips a clockcycle of setting adc_value
+        }
+        //else print out 66
+        else{
+            UCA0TXBUF = 66;
+        }
+
+          break;
+
+  }
+
+  case 4:break;    // Vector 4 - TXIFG
+  default: break;
+  }
+}
+
+
+
+
+
+
+
+
